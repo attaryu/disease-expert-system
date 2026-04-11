4f74de56b444d4b206e297f16498cc36ad5292e9
attaryu <mattarannaufal@gmail.com>
2026-04-11 14:58:34 +0700

    feat: add dynamic symptom checkboxes and result display in MainController

--- src/main/java/com/diseaseexpertsystem/MainController.java
+++ src/main/java/com/diseaseexpertsystem/MainController.java
@@ -1,7 +1,13 @@
 package com.diseaseexpertsystem;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
 public class MainController {
@@ -9,12 +15,55 @@ public class MainController {
   private VBox symptomContainer;
 
   @FXML
+  private TextArea resultDisplay;
+
+  private Map<String, CheckBox> checkBoxMap = new HashMap<>();
+
+  /* lifecycle handler */
+
+  @FXML
+  private void initialize() {
+    renderCheckBoxes();
+  }
+
+  private void renderCheckBoxes() {
+    List<String> semuaGejala = List.of(
+        "Diare",
+        "Nyeri Otot",
+        "Lelah",
+        "Sakit Tenggorokan",
+        "Hilang Indra Penciuman");
+
+    for (String gejala : semuaGejala) {
+      CheckBox cb = new CheckBox(gejala);
+
+      checkBoxMap.put(gejala, cb);
+      symptomContainer.getChildren().add(cb);
+    }
+  }
+
+  /* interaction handler */
+
+  @FXML
   void diagnoseDisease(ActionEvent event) {
-    System.out.println("Diagnosing disease...");
+    StringBuilder sb = new StringBuilder();
+    sb.append("Gejala yang dipilih:\n");
+
+    for (Map.Entry<String, CheckBox> entry : checkBoxMap.entrySet()) {
+      if (entry.getValue().isSelected()) {
+        sb.append("- ").append(entry.getKey()).append("\n");
+      }
+    }
+
+    resultDisplay.setText(sb.toString());
   }
 
   @FXML
   void resetSymptom(ActionEvent event) {
-    System.out.println("Resetting symptoms...");
+    for (CheckBox cb : checkBoxMap.values()) {
+      cb.setSelected(false);
+    }
+
+    resultDisplay.clear();
   }
 }
